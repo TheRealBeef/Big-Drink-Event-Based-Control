753752ec7aeab14105369d1d9863bcd3357a2cf2
Mateusz Popiel <popielmateusz12@gmail.com>
2022-11-23 06:23:46 +0100

    minor fix: added shared workspace to a docker run script

--- pub-sub/run.sh
+++ pub-sub/run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo
+  echo "Usage: "$(basename "$0")" path_to_ros2_workspace"
+  echo
+  exit 1
+fi
+
+[ ! -d "$1" ] && echo "Directory '$1' DOES NOT exists"
+ 
+WS_PATH=$(readlink -f $1)
+
 scriptDir=$(dirname $0 | xargs -i readlink -f {})
+
 docker run --rm \
     --privileged \
     --env DISPLAY \
@@ -7,6 +20,7 @@ docker run --rm \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.ssh:/home/developer/.ssh \
     -v "$scriptDir/shared/:/projects" \
+    -v $WS_PATH:/root/ws \
     --privileged \
     --net=host \
     --hostname $(hostname) \
