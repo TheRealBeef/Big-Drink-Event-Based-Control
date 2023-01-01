43708000e4919ec455866a7091bb36e19984be8f
Mateusz Popiel <popielmateusz12@gmail.com>
2023-01-01 22:50:31 +0100

    Drink mixer can handle an order, robotic arm is simulated

--- ws/README.md
+++ ws/README.md
@@ -1,39 +1,38 @@
-# ROS 2 Humble + Gazebo
+# Big Drink Project
 
-After following intructions you will have ROS2 up and running and Gazebo environement ready to go.
+## ROS Melodic
 
-## Set up ROS2 with Gazebo
-
-Only one step here. The command put below will build ROS2 image with a Dockerfile, which will take care of setting it up. It can take a while since it will execute ```apt update``` command and download Gazebo.
+Create a docker image, its name will be `osrf/ros:melodic-rpe`. You can have any other name, just be sure to change it in `run` and `attach` scripts.
 
 ```bash
-  docker build -t osrf/ros:humble-gazebo .
-```
-
-## Run your docker container
-
-After using a `run` script your ros2 is ready to use.
-
-```bash
+  docker build -t osrf/ros:melodic-rpe .
   . run .
 ```
 
-In my opinion a good practice is to run a roscore command on this terminal, open a new one, attach it to the container and do not touch the one running a roscore.
+It is a good practice to run a `roscore` command on this terminal, open a new one, attach it to the container and do not touch the one running a roscore. Although it is not necessery. `open.launch` file is used in a project, if there is no roscore running it will run it automatically.
 
 ```bash
   roscore
 ```
 
-Open a new terminal.
+To work on a different terminaluse `attach` script.
 
 ```bash
   . attach
 ```
 
-To start a restaurant must be open.
+## Running a restaurant
+
+To start a restaurant must be open. Just run an `open` script.
 
 ```bash
   . open
+```
+
+After tou are done close a restaurant with a `close` script.
+
+```bash
+  . close
 ```
 
 ## Designing a node
@@ -42,7 +41,11 @@ All nodes should be run from a `open.launch` file.
 
 ## Customer
 
-A customer is a user and can order a drink executing an `order` script.
+A customer is a user and can order a drink executing an `order` script. They can check this order status by subscribing an `order_status` topic
+
+```bash
+rosropic echo order_status
+```
 
 ## Drink Mixer
 
@@ -52,7 +55,6 @@ Drink mixer is a ros node that uses following topics:
 
 - Subscribes:
   - /order - String,
-    - TO DO
   - /glass_state - String:
     - Empty Glass Ready,
     - No Glass
@@ -65,7 +67,7 @@ Drink mixer is a ros node that uses following topics:
     - No Glass,
   - /order_status - String,
 
-It takes any order from `/order` topic and puts it into a FIFO `queue` which maximum size is defined in a `open.launch` file. While it is possible it serves the order and updates its state in `/drink_mixer` topic. Four states are defined. In a meantime it keeps a customer updated and publishes string messages in `/order_status` topic.
+It takes any order from `/order` topic and puts it into a list which size is defined in an `open.launch` file (1 in this commit). If possible it serves the order and updates its state in `/drink_mixer` topic. Four states are defined. In a meantime it keeps a customer updated and publishes string messages in `/order_status` topic.
 
 ## Stationary Robotic Arm
 
@@ -74,3 +76,10 @@ It takes any order from `/order` topic and puts it into a FIFO `queue` which max
 - Check valid `/glass_state` messages. If invalid message was sent, drink mixer will raise `ValueError` exception.
 - Make sure that a topic `/glass_state` is always updated.
 - Drink Ready is a message from a `/drink_mixer_topic` and not from a `/glass_state`.
+
+### Simulated topics
+
+Before a node of this component is created every importnat from a point of view of drink mixer features provided by an arm are simulated with scripts:
+
+- `pubEGR` - publishes "Empty Glass Ready" message, mxier waits for it while received an order.
+- `pubNG` - publishes "No Glass" message, when a drink is ready a robotic arm must take it, so there is no glass until it puts another one.
--- ws/src/big_drink/scripts/drink_mixer.py
+++ ws/src/big_drink/scripts/drink_mixer.py
@@ -6,6 +6,11 @@ from time import sleep
 import rospy
 from std_msgs.msg import String, Bool
 
+class Order:
+  def __init__(self, name="Free", nr=0):
+    self.name = name
+    self.nr = nr
+
 class DrinkMixer:
   
   def __init__( self, max_orders ):
@@ -19,101 +24,175 @@ class DrinkMixer:
     self.pub_mixer_status = rospy.Publisher('drink_mixer_state', String, queue_size=10)
     self.pub_order_status = rospy.Publisher('order_status', String, queue_size=10)
     
-    self.orders = [ "Free" for i in range(max_orders) ]
+    self.orders = [ Order("Free",i) for i in range(max_orders) ]
+    self.current_order = self.orders[0]
     self.glass_state = "No Glass"
     self.glass_available = False
     self.should_run = True
+    self.should_serve_order = False
+    self.order_waiting = False
+    self.drink_handled = False
     self.current_state = "Idle"
-
-    self.update_state()
   
 
   def order_callback(self,order):
 
+    log_str = " Order \"" + order.data + "\" received"
+    rospy.loginfo(rospy.get_caller_id() + log_str)
+
     if not self.can_take_order():
-      msg = "There is already " + len(self.orders) + " orders that must be served."
+      msg = "There is already " + str(len(self.orders)) + " orders that must be served."
       self.pub_order_status.publish(msg)
       rospy.loginfo(rospy.get_caller_id() + " Max number of orders reached!")
 
     else:
-      nr = self.take_order(order.data)
-      msg = "Your order \"" + order.data + "\" has a number " + nr+1 + "."
+      nr = self.collect_order(order.data)
+      msg = "Your order \"" + order.data + "\" has a number " + str(nr+1) + "."
       self.pub_order_status.publish(msg)
-      log_str = " Order \"" + order.data + "\" received. Number " + nr+1 + " assigned"
+      log_str = " Order \"" + order.data + "\" received. Number " + str(nr+1) + " assigned"
       rospy.loginfo(rospy.get_caller_id() + log_str)
+      if self.current_state == "Idle":
+        if self.glass_state == "No Glass":
+          self.current_state = "No Glass"
+          self.update_state()
+        elif self.glass_state == "Empty Glass Ready":
+          self.should_serve_order = True
+        else:
+          self.invalid_glass_state()
 
 
   def glass_state_callback(self,glass_state):
 
+    log_str = " Message \"" + glass_state.data + "\" received"
+    rospy.loginfo( rospy.get_caller_id() + log_str )
+
     self.glass_state = glass_state.data
-    if self.glass_state == "Empty Glass Ready":
+    if self.current_state == "Drink Ready":
+      self.drink_handled = True
+    elif self.glass_state == "Empty Glass Ready":
       self.glass_available = True
+      if self.current_state == "No Glass":
+        self.should_serve_order = True
     elif self.glass_state == "No Glass":
       self.glass_available = False
+      if self.current_state == "Drink Ready":
+        self.current_state = "Idle"
+        self.update_state()
     else:
-      err_msg = \
-      "Invalid glass state: \"" + self.glass_state + \
-      "\". Valid states are:\n" + \
-      "\t- \"Empty Glass Ready\" " + \
-      "\t- \"No Glass\" "
-      raise ValueError(err_msg)
+      self.invalid_glass_state()
 
 
   def close_callback(self, must_end):
 
     self.should_run = not must_end.data
-    rospy.loginfo(rospy.get_caller_id() + " Process must finish: " + str(must_end.data) + "." )
-  
+    log_str = " Process must finish: " + str(must_end.data) + "."
+    rospy.loginfo(rospy.get_caller_id() + log_str )
+
+
   def serve_order(self):
-    #TODO: CHANGE IT 
-    order = self.orders.get()
+
+    self.current_order = self.take_order()
     self.current_state = "Drink Pouring"
     self.update_state()
-    self.pub_order_status.publish("Your drink from \"" + order + "\" is getting ready!")
+    msg = "Your drink from order nr " + \
+      str(self.current_order.nr +1) + ": '" + \
+      self.current_order.name + \
+      "' is getting ready!"
+    self.pub_order_status.publish(msg)
     sleep(2)
     self.current_state = "Drink Ready"
-    self.pub_order_status.publish("Your drink from \"" + order + "\" is ready!")
+    self.update_state()
+    msg = "Your drink from order nr " + \
+      str(self.current_order.nr + 1) + ": '" + \
+      self.current_order.name + "' is ready!"
+    self.pub_order_status.publish(msg)
 
 
   def update_state(self):
 
     self.pub_mixer_status.publish(self.current_state)
-    rospy.loginfo(rospy.get_caller_id() + " Status: " + self.current_state + ".")
+    log_str = " Change state to " + self.current_state + "."
+    rospy.loginfo(rospy.get_caller_id() + log_str)
 
 
   def can_take_order(self):
-    if "Free" in self.orders:
-      return True
-    else:
-      return False
+    for order in self.orders:
+      if order.name == "Free":
+        return True
+    return False
 
-  def take_order(self, order):
-    id = self.orders.index("Free")
-    self.order[id] = order
-    return id
+
+  def collect_order(self, order_name):
+    for order in self.orders:
+      if order.name == "Free":
+        order.name = order_name
+        return order.nr
+      else:
+        order_found = False
+    if not order_found:
+      rospy.logerr(rospy.get_caller_id() + " No order with a name " + order_name)
+      raise ValueError("No order with a name " + order_name)
+
+
+  def take_order(self):
+    for order in self.orders:
+      if order.name != "Free":
+        return order
+    rospy.logerr(rospy.get_caller_id() + " No order to serve!")
+    raise ValueError("No order to serve!")
 
 
   def no_order(self):
-    for i in range(len(self.orders)):
-      if not self.orders[i] == "Free":
+    for order in self.orders:
+      if order.name != "Free":
         return False
     return True
 
 
+  def clean_order(self):
+    for order in self.orders:
+      if order.nr == self.current_order.nr:
+        order.name = "Free"
+        log_str = " Number " + str(order.nr+1) + "can be assigned to an order."
+        rospy.loginfo(rospy.get_caller_id() + log_str)
+        return
+    log_str = " Can't clean order nr " + str(self.current_order.nr) + \
+    ". It doesn't match abailable order numbers."
+    rospy.logerr(rospy.get_caller_id() + log_str)
+    raise ValueError(log_str)
+
+
+  def invalid_glass_state(self):
+    err_msg = \
+    "Invalid glass state: \"" + self.glass_state + \
+    "\". Valid states are:\n" + \
+    "\t- \"Empty Glass Ready\" " + \
+    "\t- \"No Glass\" "
+    raise ValueError(err_msg)
+
+
   def run(self):
 
-    # TODO: IT ITERATES EVERY 0.001s!!
+    self.update_state()
     while self.should_run:
-      if self.no_order():
-        self.current_state = "Idle"
-      elif not self.glass_available:
-        self.current_state = "No Glass"
-      elif self.current_state == "Drink Ready":
-        sleep(0.01)
-      else:
+      sleep(0.1)
+      if self.should_serve_order:
+        self.should_serve_order = False
         self.serve_order()
-
-      self.update_state()
+        # Drink ready
+        while not self.drink_handled:
+          # Wait for robotic arm
+          sleep(0.1)
+        self.clean_order()
+        if self.no_order():
+          self.current_state = "Idle"
+          self.update_state()
+        else:
+          if self.glass_available:
+            self.should_serve_order = True
+          else:
+            self.current_state = "No Glass"
+            self.update_state()
 
     rospy.loginfo(rospy.get_caller_id() + " Ending a process")
 
